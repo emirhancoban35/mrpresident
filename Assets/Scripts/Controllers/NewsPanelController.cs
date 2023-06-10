1c0a552c34bf67d626d10822c90991251631f102
emirhancoban35 <emirhancobann35@gmail.com>
2023-06-10 16:08:05 +0300

    create SetSizePanel

--- Assets/Scripts/Controllers/NewsPanelController.cs
+++ Assets/Scripts/Controllers/NewsPanelController.cs
@@ -6,20 +6,33 @@ using UnityEngine;
 public class NewsPanelController : MonoSingleton<NewsPanelController>
 {
     private RectTransform _newsPanel;
+    private RectTransform _panel;
+    private Transform _newsPanelTransform;
+
     private void Start()
     {
         _newsPanel = GameObject.Find("NewsPanel").GetComponent<RectTransform>();
+        _panel = GameObject.Find("Panel").GetComponent<RectTransform>();
+        _newsPanelTransform = _newsPanel.transform;
     }
 
-    public void NewNews()
+    public Transform GetPanelLocation()
     {
-        var currentPosition = _newsPanel.localPosition;
+        return _newsPanelTransform;
+    }
+    
+    public Transform GetNewsPanelLocation()
+    {
+        return _panel.transform;
+    }
+
+    public void SetSizePanel()
+    {
         var currentScale = _newsPanel.localScale;
-        var currentLocation = _newsPanel.localRotation;
-        currentPosition.y += 5f;
-        currentPosition.y += 10f;
-        currentLocation.y -= 5f;
-        _newsPanel.localPosition= currentPosition;
+        var currentPositionY = _newsPanel.anchoredPosition.y;
+        currentScale.y += 0.1f;
+        float newYPosition = currentPositionY + 57f;
+        _newsPanel.anchoredPosition = new Vector2(_newsPanel.anchoredPosition.x, newYPosition);
         _newsPanel.localScale = currentScale;
     }
 }
--- Assets/Scripts/NewsCreator.cs
+++ Assets/Scripts/NewsCreator.cs
@@ -9,25 +9,19 @@ public class NewsCreator : MonoBehaviour
 {
     public List<News> newsList = new List<News>();
     public GameObject newsPrefab;
-    private RectTransform _createdTransform;
+    private Transform _createdTransform;
     private GameObject _panel;
     public List<GameObject> newsPrefabs = new List<GameObject>();
-    public NewsPanelController newsPanelController;
-
-    private void Start()
-    {
-        _panel = GameObject.FindWithTag("Panel"); 
-    }
     
-
     public void CreateNews()
     {
-
+        
         int randomNewsNumber = Random.Range(0, newsList.Count);
+       
         News createdNews = newsList[randomNewsNumber];
         
-        GameObject instantiatedPrefab = Instantiate(newsPrefab , _panel.transform);
-    
+        GameObject instantiatedPrefab = Instantiate(newsPrefab ,NewsPanelController.Instance.GetPanelLocation());
+        
         foreach (var news in newsPrefabs)   
         {
             var newsTransform = news.GetComponent<RectTransform>();
@@ -35,14 +29,14 @@ public class NewsCreator : MonoBehaviour
         }
         newsPrefabs.Add(instantiatedPrefab);
         
-        instantiatedPrefab.transform.parent = _panel.transform;
-
+        instantiatedPrefab.transform.parent = NewsPanelController.Instance.GetNewsPanelLocation();
+        
         Text textComponent = instantiatedPrefab.GetComponentInChildren<Text>();
-
+        
         textComponent.text = createdNews.newsText;  
         
         newsList.RemoveAt(randomNewsNumber);
         
-        newsPanelController.NewNews();
+        NewsPanelController.Instance.SetSizePanel();
     }
 }
