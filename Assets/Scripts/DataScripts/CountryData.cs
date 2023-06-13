c5b778ba7967a996099c81488f782527d3116b0c
emirhancoban35 <emirhancobann35@gmail.com>
2023-06-13 23:06:24 +0300

    international relations structure was established

--- Assets/Scripts/DataScripts/CountryData.cs
+++ Assets/Scripts/DataScripts/CountryData.cs
@@ -25,4 +25,42 @@ public class CountryData : ScriptableObject
     public List<Countries> allies = new List<Countries>();
     
     public List<Countries> enemies = new List<Countries>();
+    
+    [System.Serializable]   
+    public class CountryRelationship
+    {
+        public Countries country;
+        public int relationship;
+
+        public CountryRelationship(Countries country, int relationship)
+        {
+            this.country = country;
+            this.relationship = relationship;
+        }
+    }
+
+    public List<CountryRelationship> relationships = new List<CountryRelationship>();
+
+    public void SetRelationship(Countries otherCountry, int relationship)
+    {
+        CountryRelationship existingRelationship = relationships.Find(r => r.country == otherCountry);
+        if (existingRelationship != null)
+        {
+            existingRelationship.relationship = Mathf.Clamp(relationship, 0, 100);
+        }
+        else
+        {
+            relationships.Add(new CountryRelationship(otherCountry, Mathf.Clamp(relationship, 0, 100)));
+        }
+    }
+
+    public int GetRelationship(Countries otherCountry)
+    {
+        CountryRelationship existingRelationship = relationships.Find(r => r.country == otherCountry);
+        if (existingRelationship != null)
+        {
+            return existingRelationship.relationship;
+        }
+        return 0;
+    }
 }
--- Assets/Scripts/NewsCreator.cs
+++ Assets/Scripts/NewsCreator.cs
@@ -1,58 +1,68 @@
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NewsCreator : MonoBehaviour
 {
-    #region Varabiles
     public List<News> newsList = new List<News>();
-    public GameObject newsPrefab;
     public List<GameObject> newsPrefabs = new List<GameObject>();
-    #endregion
 
-    #region Functions
+    [SerializeField] private GameObject newsPrefab;
+    [CanBeNull] private News newsToBePublished;
+
+    public void SetNewsToBePublished(News news)
+    {
+        newsToBePublished = news;
+    }
+
+    public void AddNews(News news)
+    {
+        newsList.Add(news);
+    }
+
+    public void RemoveNews(News news)
+    {
+        newsList.Remove(news);
+    }
+
     public void CreateNews()
     {
-        int randomNewsNumber = Random.Range(0, newsList.Count);
-        // if (newsList[randomNewsNumber] == null)
-        //     return;
-        
-        News createdNews = newsList[randomNewsNumber];
-
-        var contains = createdNews.newsOfWhichCountries.Contains(GameManager.Instance.myCountry.countryName);
-        
-        if (contains)
+        if (newsToBePublished != null)
         {
-            
-            GameObject instantiatedPrefab = Instantiate(newsPrefab ,NewsPanelController.Instance.GetPanelLocation());
-        
-            foreach (var news in newsPrefabs)   
-            {
-                var newsTransform = news.GetComponent<RectTransform>();
-
-                var currentPosition = newsTransform.anchoredPosition;
-
-                currentPosition.y += 200;
-
-                newsTransform.anchoredPosition = currentPosition;
-            }
-            newsPrefabs.Add(instantiatedPrefab);
-        
-            instantiatedPrefab.transform.parent = NewsPanelController.Instance.GetNewsPanelLocation();
-        
-            Text textComponent = instantiatedPrefab.GetComponentInChildren<Text>();
-        
-            textComponent.text = createdNews.newsText;  
-            newsList.RemoveAt(randomNewsNumber);
-
+            PublishNews(newsToBePublished);
+            newsToBePublished = null;
         }
         else
         {
-            CreateNews();
-            return;
-        }
-        // NewsPanelController.Instance.SetSizePanel();
-    }
-    #endregion
+            int randomNewsNumber = Random.Range(0, newsList.Count);
+            News createdNews = newsList[randomNewsNumber];
 
+            var contains = createdNews.newsOfWhichCountries.Contains(GameManager.Instance.myCountry.countryName);
+
+            if (contains)
+            {
+                PublishNews(createdNews);
+                newsList.RemoveAt(randomNewsNumber);
+            }
+        }
+    }
+
+    private void PublishNews(News selectedNews)
+    {
+        GameObject instantiatedPrefab = Instantiate(newsPrefab, NewsPanelController.Instance.GetPanelLocation());
+        newsPrefabs.Add(instantiatedPrefab);
+
+        foreach (var news in newsPrefabs)
+        {
+            var newsTransform = news.GetComponent<RectTransform>();
+            var currentPosition = newsTransform.anchoredPosition;
+            currentPosition.y += 200;
+            newsTransform.anchoredPosition = currentPosition;
+        }
+
+        instantiatedPrefab.transform.SetParent(NewsPanelController.Instance.GetNewsPanelLocation(), false);
+        Text textComponent = instantiatedPrefab.GetComponentInChildren<Text>();
+        textComponent.text = selectedNews.newsText;
+    }
 }
